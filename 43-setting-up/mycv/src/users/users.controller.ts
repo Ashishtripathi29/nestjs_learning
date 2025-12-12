1d82aba8d34b8309f6f35b501dfa6ac154e828b8
Ashish Tripathi <ashishtripathipara@gmail.com>
2025-12-13 00:25:32 +0530

    crud operation

--- 43-setting-up/mycv/src/users/users.controller.ts
+++ 43-setting-up/mycv/src/users/users.controller.ts
@@ -1,11 +1,47 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Param,
+  Patch,
+  Query,
+  Delete,
+  Get,
+  UseInterceptors,
+//   ClassSerializerInterceptor,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private userService: UsersService) {}
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log(body);
-    return body;
+    return this.userService.createUser(body.email, body.password);
+  }
+
+//   @UseInterceptors(ClassSerializerInterceptor)
+  @Get('/alluser')
+  findAllUser(@Query('email') email: string) {
+    return this.userService.find(email);
+  }
+
+//   @UseInterceptors(ClassSerializerInterceptor) // this is for object to plane json
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    console.log(id);
+    return this.userService.findOne(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.userService.update(parseInt(id), body);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.userService.remove(parseInt(id));
   }
 }
